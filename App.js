0fcda3c097ab0798034afd4051535500162a97d7
Willian Watanabe <watinha@gmail.com>
2020-11-19 17:53:57 -0300

    configuring bottom-tab router for app

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export function DummyComponent1 () {
+  return (
+    <View style={{backgroundColor: '#FFCCCC', ...styles.dummy}}>
+      <Text>Dummy 1!!!</Text>
+    </View>
+  );
+}
+export function DummyComponent2 () {
+  return (
+    <View style={{backgroundColor: '#AACCFF', ...styles.dummy}}>
+      <Text>Dummy 2!!!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Dummy 1'
+                    component={DummyComponent1}></Tab.Screen>
+        <Tab.Screen name='Dummy 2'
+                    component={DummyComponent2}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  dummy: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center'
+  }
 });
--- __tests__/App.js
+++ __tests__/App.js
@@ -1,10 +1,39 @@
-import 'react-native';
+import { View } from 'react-native';
 import React from 'react';
+
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { render, fireEvent, cleanup } from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+jest.useFakeTimers();
+afterEach(cleanup);
+
+// Silence the warning https://github.com/facebook/react-native/issues/11094#issuecomment-263240420
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+it('renders correctly', async () => {
+  let { queryAllByText } = render(<App />),
+      containers, components1, components2;
+
+  components1 = await queryAllByText('Dummy 1!!!');
+  components2 = await queryAllByText('Dummy 2!!!');
+
+  expect(components1.length).toBe(1);
+  expect(components2.length).toBe(0);
+});
+
+it('changes visible view element after press', async () => {
+  let { queryByText, queryAllByText } = render(<App />),
+      containers, components1, components2;
+
+  tabItem2 = await queryByText('Dummy 2');
+
+  await fireEvent(tabItem2, 'press');
+
+  components1 = await queryAllByText('Dummy 1!!!');
+  components2 = await queryAllByText('Dummy 2!!!');
+
+  expect(components1.length).toBe(1);
+  expect(components2.length).toBe(1);
 });
