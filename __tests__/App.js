bf59e1fcd6ee2be920f00660e37fa0a1c46810af
Willian Watanabe <watinha@gmail.com>
2020-11-30 19:47:46 -0300

    migrating from testing-library -> react-test-renderer

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,45 +1,36 @@
 import { View } from 'react-native';
 import React from 'react';
+import { create, act } from 'react-test-renderer';
 
-import App from '../App';
-import fs from 'fs';
-
-// Note: test renderer must be required after react-native.
-import { act, render, fireEvent, cleanup, waitForElementToBeRemoved } from '@testing-library/react-native';
+import App, { DummyComponent1, DummyComponent2 } from '../App';
 
 jest.useFakeTimers();
-afterEach(cleanup);
 
 // Silence the warning https://github.com/facebook/react-native/issues/11094#issuecomment-263240420
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 
 it('renders correctly', async () => {
-  let target = render(<App />),
-      containers, components1, components2;
-  let { queryAllByText } = target;
-
-  await act(async () => {
-    components1 = await queryAllByText('Dummy 1!!!');
-    components2 = await queryAllByText('Dummy 2!!!');
-  });
+  let target = create(<App />),
+      components1 = target.root.findAllByType(DummyComponent1),
+      components2 = target.root.findAllByType(DummyComponent2);
 
   expect(components1.length).toBe(1);
   expect(components2.length).toBe(0);
+  target.unmount();
 });
 
 it('changes visible view element after press', async () => {
-  let target = render(<App />),
-      containers, components1, components2;
-  let { queryByText, queryAllByText } = target;
+  let target = create(<App />), components1, components2;
+  const tabs = target.root.findAll((el) =>
+    el.instance && el.props.onPress &&
+    el.props.accessibilityRole === 'button');
 
-  await act(async () => {
-    tabItem2 = await queryByText('Dummy 2');
-    fireEvent.press(tabItem2);
-  });
+  act(tabs[1].props.onPress);
 
-  components1 = await queryAllByText('Dummy 1!!!');
-  components2 = await queryAllByText('Dummy 2!!!');
+  components1 = target.root.findAllByType(DummyComponent1);
+  components2 = target.root.findAllByType(DummyComponent2);
 
   expect(components1.length).toBe(1);
   expect(components2.length).toBe(1);
+  target.unmount();
 });
