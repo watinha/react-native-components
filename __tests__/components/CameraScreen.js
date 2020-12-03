86e96e4c79466021db8cf5fb0f43edb194ce9330
Willian Watanabe <watinha@gmail.com>
2020-12-03 14:45:11 -0300

    camera implementation (WIP: migrating to redux and adjusting layout)

--- __tests__/components/CameraScreen.js
+++ __tests__/components/CameraScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { act, create } from 'react-test-renderer';
+import fs from 'expo-file-system';
 
 import App from '../App';
 
@@ -10,6 +11,7 @@ jest.useFakeTimers();
 
 // Silence the warning https://github.com/facebook/react-native/issues/11094#issuecomment-263240420
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+jest.mock('expo-file-system');
 
 it('should ask for permissions before rendering', async () => {
   let CameraMock = {
@@ -50,4 +52,104 @@ it('should not render camera if permissions are not granted', async () => {
 
   expect(CameraMock.requestPermissionsAsync.mock.calls.length).toBe(1);
   expect(camera_element.length).toBe(0);
+});
+
+it('should capture picture when button is pressed', async () => {
+  let CameraMock = {
+        requestPermissionsAsync: jest.fn(),
+        takePictureAsync: jest.fn()
+      },
+      rendered_test = null,
+      button = null;
+  CameraMock.requestPermissionsAsync.mockResolvedValue({status: true});
+  CameraMock.takePictureAsync.mockResolvedValue({
+    'height': 1000,
+    'width': 800,
+    'uri': 'file:///var/mobile/somewhere'
+  });
+  fs.moveAsync.mockReturnValue('');
+  fs.readAsStringAsync.mockImplementation(() => { throw {}; });
+  fs.writeAsStringAsync.mockReturnValue('');
+
+  await act(async () =>
+    rendered_test = await create(
+      <CameraScreen camera={CameraMock}></CameraScreen>));
+
+  button = rendered_test.root.find((el) =>
+    el.props.testID === 'camera_button');
+
+  await act(async () =>
+    button.props.onPress());
+
+  expect(fs.moveAsync).toBeCalledWith(
+    'file:///var/mobile/somewhere',
+    `${fs.documentDirectory}/1.png`);
+  expect(fs.writeAsStringAsync).toBeCalledWith(
+    `${fs.documentDirectory}/pictures.json`,
+    JSON.stringify({
+      count: 1,
+      pictures: [
+        { uri: `${fs.documentDirectory}/1.png`, height: 1000, width: 800 }
+      ]
+    }));
+  expect(fs.readAsStringAsync).toBeCalledWith(
+    `${fs.documentDirectory}/pictures.json`);
+
+  fs.moveAsync.mockClear();
+  fs.readAsStringAsync.mockClear();
+  fs.writeAsStringAsync.mockClear();
+});
+
+it('should capture picture when pressed a second time', async () => {
+  let CameraMock = {
+        requestPermissionsAsync: jest.fn(),
+        takePictureAsync: jest.fn()
+      },
+      rendered_test = null,
+      button = null;
+  CameraMock.requestPermissionsAsync.mockResolvedValue({status: true});
+  CameraMock.takePictureAsync.mockResolvedValue({
+    'height': 755,
+    'width': 255,
+    'uri': 'file:///var/another/file'
+  });
+  fs.moveAsync.mockReturnValue('');
+  fs.readAsStringAsync.mockResolvedValue(
+    JSON.stringify({
+      count: 1,
+      pictures: [
+        { uri: '1.png', height: 1, width: 2 }
+      ]
+    })
+  );
+  fs.writeAsStringAsync.mockReturnValue('');
+
+  await act(async () =>
+    rendered_test = await create(
+      <CameraScreen camera={CameraMock}></CameraScreen>));
+
+  button = rendered_test.root.find((el) =>
+    el.props.testID === 'camera_button');
+
+  await act(async () =>
+    button.props.onPress());
+
+  expect(fs.moveAsync).toBeCalledWith(
+    'file:///var/another/file',
+    `${fs.documentDirectory}/2.png`);
+  expect(fs.writeAsStringAsync).toBeCalledWith(
+    `${fs.documentDirectory}/pictures.json`,
+    JSON.stringify({
+      count: 2,
+      pictures: [
+        { uri: `${fs.documentDirectory}/2.png`, height: 755, width: 255 },
+        { uri: '1.png', height: 1, width: 2 }
+      ]
+    }));
+  expect(fs.readAsStringAsync).toBeCalledWith(
+    `${fs.documentDirectory}/pictures.json`);
+
+  fs.moveAsync.mockClear();
+  fs.readAsStringAsync.mockClear();
+  fs.writeAsStringAsync.mockClear();
 });
--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,31 +1,65 @@
 import React, { useState, useEffect } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 
 import { Camera } from 'expo-camera';
+import * as fs from 'expo-file-system';
 
 export default function CameraScreen ({ camera }) {
   let [permission, setPermission] = useState(false),
+      [count, setCount] = useState(0),
+      [pictures, setPictures] = useState([]),
       camera_api = camera ? camera : Camera;
 
   useEffect(() => {
     (async () => {
       const permissions = await camera_api.requestPermissionsAsync();
       setPermission(permissions.status);
+
+      try {
+        const json = JSON.parse(await fs.readAsStringAsync(
+          `${fs.documentDirectory}/pictures.json`));
+        setCount(json.count);
+        setPictures(json.pictures);
+      } catch (e) {
+        throw e;
+      }
     })();
   }, []);
+
+  const __onPress = async () => {
+    const photo = await camera_api.takePictureAsync();
+    fs.moveAsync(photo['uri'], `${fs.documentDirectory}/${count + 1}.png`);
+    fs.writeAsStringAsync(
+      `${fs.documentDirectory}/pictures.json`,
+      JSON.stringify({
+        count: (count + 1),
+        pictures: [{
+          uri: `${fs.documentDirectory}/${count + 1}.png`,
+          height: photo['height'],
+          width: photo['width']
+        }, ...pictures]
+      })
+    );
+    setCount(count + 1);
+  };
 
   if (!permission) {
     return (
       <View>
         <Text testID="no_camera_message">
-          Camera permissions were not granted!!!</Text>
+          Camera permissions were not granted!!!
+        </Text>
       </View>
     );
   }
   return (
     <View style={{flex: 1}}>
-      <Camera style={{flex: 1}}testID="camera_element"></Camera>
+      <Camera style={{flex: 1}} testID="camera_element"></Camera>
+      <TouchableHighlight testID="camera_button"
+                          onPress={__onPress}>
+        <Text>Oi</Text>
+      </TouchableHighlight>
     </View>
   );
 }
